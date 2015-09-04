c05b12a356279a70ccc471b5b4cd9c4b6978be5c
mausv <mausv@me.com>
2015-09-04 12:44:25 -0500

    Added int variables

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,6 +3,17 @@
  */
 public class Hello {
     public static void main(String[] args) {
+
         System.out.println("Hello, World!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myTotalThousand = 1000 - myTotal;
+
+        System.out.println(myTotalThousand);
     }
 }

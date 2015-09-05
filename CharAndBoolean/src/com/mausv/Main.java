e15f3121a2f181252efa94b1aeff6ac84ed8762b
mausv <mausv@me.com>
2015-09-04 23:21:39 -0500

    Added Char and Boolean

---
+++ CharAndBoolean/src/com/mausv/Main.java
@@ -0,0 +1,15 @@
+package com.mausv;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char myChar = '\u00A9';
+        System.out.println("Unicode output was: " + myChar);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+
+        char regMark = '\u00AE';
+        System.out.println(regMark);
+    }
+}

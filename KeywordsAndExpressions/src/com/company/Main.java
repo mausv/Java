115ba1810dcf32aabef3cac8e96742019c6f1900
mausv <mausv@me.com>
2015-09-06 22:27:57 -0500

    Added expressions

---
+++ KeywordsAndExpressions/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // The expressions are the lines in the comment lines
+
+        int score = 100; // score = 100
+        if (score > 99) { // score > 99
+            System.out.println("You got the highscore!"); // "You got the highscore!"
+            score = 0; // score = 0
+        }
+    }
+}

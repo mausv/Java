914f05b997eb681bd5b433346fbe09128551d550
mausv <mausv@me.com>
2015-09-06 22:38:08 -0500

    Added white spaces, indeting and statements

---
+++ StatementsWhiteSpaceAndIndenting/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50; // Statement
+        myVariable++; // Statement
+        System.out.println("Test"); // Statement
+        System.out.println("This is" +
+                            "another" +
+                            "test."); // All this is a statement
+
+        int anotherVariable = 50; myVariable--;System.out.println("Another one");// You can put statements in the same line as long as you have the separator ;
+        
+    }
+}

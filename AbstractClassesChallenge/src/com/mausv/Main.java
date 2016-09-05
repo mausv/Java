f876d21fe84ed8f698945024748c979dd7f537dc
mausv <mausv@me.com>
2016-09-05 09:50:56 -0500

    Added Binary tree

--- AbstractClassesChallenge/src/com/mausv/Main.java
+++ AbstractClassesChallenge/src/com/mausv/Main.java
@@ -3,19 +3,19 @@ package com.mausv;
 public class Main {
 
     public static void main(String[] args) {
-        MyLinkedList list = new MyLinkedList(null);
-        list.tranverse(list.getRoot());
+        SearchTree tree = new SearchTree(null);
+        tree.tranverse(tree.getRoot());
 
         String stringData = "9 7 5 8 4 2 1 0 3 2";
 
         String[] data = stringData.split(" ");
         for(String s : data) {
             // Create new item with value set to the string s
-            list.addItem(new Node(s));
+            tree.addItem(new Node(s));
         }
 
-        list.tranverse(list.getRoot());
-        list.removeitem(new Node("3"));
-        list.tranverse(list.getRoot());
+        tree.tranverse(tree.getRoot());
+        tree.removeitem(new Node("3"));
+        tree.tranverse(tree.getRoot());
     }
 }
